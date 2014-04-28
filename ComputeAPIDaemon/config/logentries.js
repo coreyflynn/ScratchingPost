8de996b6320ba878d5ee18c29e076145af28383b
Corey Flynn <flynn.corey@gmail.com>
2014-04-28 18:15:40 -0400

    cleared repo

--- ComputeAPIDaemon/config/logentries.js
+++
@@ -1,6 +0,0 @@
-var logentries = require('node-logentries');
-var log = logentries.logger({
-  token:'f0d5ca99-119e-44f6-8544-c56b085d6675'
-});
-
-module.exports = {log: log};
--- ComputeAPISubmission/config/mongo.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    url: 'mongodb://admin:adminpass@23.23.153.188:27017/tool,'+
-            'mongodb://admin:adminpass@107.22.174.155:27017'
-}
