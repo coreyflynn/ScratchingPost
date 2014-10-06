753714ff3ef74c90f61d14bae2addf0a6d358779
Corey Flynn <flynn.corey@gmail.com>
2014-10-06 17:39:02 -0400

    updates to landing app

--- landing_app/routes/orientation.js
+++ landing_app/routes/orientation.js
@@ -1,8 +1,8 @@
 module.exports = function(req, res){
     var page = req.params.page;
     if (!req.query.module){
-        res.render('orientation/index');
+        res.render('orientation/index',{title:'CLUE Orientation'});
     }else{
-        res.render('orientation/' + req.query.module);
+        res.render('orientation/' + req.query.module, {title: 'CLUE ' + req.query.module.slice(3,req.query.module.length)});
     }
 };
